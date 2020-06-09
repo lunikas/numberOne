e46f17385513f3d7073f28eefa27e26fee95924b
JurgitaVisa <jurgita.visakaviciute@gmail.com>
2020-06-09 22:28:00 +0300

    Paskutinis pazaidimas pries miega

--- about.php
+++ about.php
@@ -3,10 +3,18 @@
 
 <div class="container about">
 <h1>About Us</h1>
-<img src="https://picsum.photos/500/500" alt="kazkas grazaus">
 
-<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus eius, voluptatibus totam explicabo voluptatem nihil nisi consequuntur adipisci laboriosam debitis, rerum necessitatibus eligendi! Cum nam ipsum magni omnis. Iusto, itaque?</p>
+<p>Mes mylim suniukus ir....</p>
+<img src="https://picsum.photos/id/237/500/500" alt="kazkas grazaus">
+
+<p>.... mokam lotyniskai ;) Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus eius, voluptatibus totam explicabo voluptatem nihil nisi consequuntur adipisci laboriosam debitis, rerum necessitatibus eligendi! Cum nam ipsum magni omnis. Iusto, itaque?</p>
+
+<p>Mus rasite Vilniaus centre</p>
 
 </div>
+
+
+
+<iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2306.4099714932604!2d25.27833385059149!3d54.68481288121817!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dd9411a1bd7cdb%3A0x645ec2d88e118d0c!2sLabdari%C5%B3%20g.%203%2C%20Vilnius%2001120!5e0!3m2!1sen!2slt!4v1591730100877!5m2!1sen!2slt"  frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
 
 <?php include "footer.php" ?>
