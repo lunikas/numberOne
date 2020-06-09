74ac21e133bc2073c400b8a9dd761695cdc2d7f6
JurgitaVisa <jurgita.visakaviciute@gmail.com>
2020-06-09 21:26:08 +0300

    About dalis pirmas bandymas (Jurgita)

--- about.php
+++ about.php
@@ -2,8 +2,10 @@
 <?php include "header.php" ?>
 
 
+<h1>About Us</h1>
+<img src="https://picsum.photos/500/500" alt="kazkas grazaus">
 
-<!-- Čia apie puslapio vidinė dalis -->
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus eius, voluptatibus totam explicabo voluptatem nihil nisi consequuntur adipisci laboriosam debitis, rerum necessitatibus eligendi! Cum nam ipsum magni omnis. Iusto, itaque?</p>
 
 
 
