5a6ab6feb95effd4a172c001c132844bd551b4e1
lunikas <lukasnikas90@gmail.com>
2020-06-10 20:12:34 +0300

    pagination

--- news.php
+++ news.php
@@ -4,28 +4,60 @@
 
 
 <div class="container ">
-<h1>News</h1>
+    <h1 calss="align-center">News</h1>
 
-    <div class="col s12 m4 l2"><img src="https://picsum.photos/150/150" alt=""></div>
-    <div class="col s12 m4 l8" ><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium odit velit soluta quam quibusdam non totam quasi, unde facilis culpa corporis expedita architecto possimus fugiat voluptate, similique nulla accusamus eos!</p></div>
-  </div>
+    <div class="row z-depth-1">
+    <div class="col s12 m4 l2"><img src="https://picsum.photos/150/150" alt="">
+    </div>
 
-    <div class="col s12 m4 l2"><button></button></div>
-  </div>
+    <div class="col s12 m4 l8">
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium odit velit soluta quam quibusdam non totam quasi, unde facilis culpa corporis expedita architecto possimus fugiat voluptate, similique nulla accusamus eos!</p>
+        <a class="waves-effect waves-teal btn-flat">Read more</a>
+    </div>
 
-<div class="row">
-    <div class="col s12 m4 l2"><img src="https://picsum.photos/150/150" alt=""></div>
-    <div class="col s12 m4 l8"><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium odit velit soluta quam quibusdam non totam quasi, unde facilis culpa corporis expedita architecto possimus fugiat voluptate, similique nulla accusamus eos!</p></div>
-    <div class="col s12 m4 l2"><button></button></div>
-  </div>
+    <div class="col s12 m4 l2"></div>
+    </div>
+
+    <div class="row z-depth-1">
+    <div class="col s12 m4 l2"><img src="https://picsum.photos/150/150" alt="">
+    </div>
+
+    <div class="col s12 m4 l8">
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium odit velit soluta quam quibusdam non totam quasi, unde facilis culpa corporis expedita architecto possimus fugiat voluptate, similique nulla accusamus eos!</p>
+        <a class="waves-effect waves-teal btn-flat">Read more</a>
+    </div>
+
+    <div class="col s12 m4 l2"></div>
+    </div>
 
 
-<div class="row">
-    <div class="col s12 m4 l2"><img src="https://picsum.photos/150/150" alt=""></div>
-    <div class="col s12 m4 l8"><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium odit velit soluta quam quibusdam non totam quasi, unde facilis culpa corporis expedita architecto possimus fugiat voluptate, similique nulla accusamus eos!</p></div>
-    <div class="col s12 m4 l2"><button></button></div>
-  </div>
+    <div class="row z-depth-1">
+    <div class="col s12 m4 l2"><img src="https://picsum.photos/150/150" alt="">
+    </div>
+
+    <div class="col s12 m4 l8">
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium odit velit soluta quam quibusdam non totam quasi, unde facilis culpa corporis expedita architecto possimus fugiat voluptate, similique nulla accusamus eos!</p>
+        <a class="waves-effect waves-teal btn-flat">Read more</a>
+    </div>
+
+    <div class="col s12 m4 l2"></div>
+    </div>
+
+
+    <div>
+    <ul class="pagination center">
+    <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
+    <li class="active"><a href="#!">1</a></li>
+    <li class="waves-effect"><a href="#!">2</a></li>
+    <li class="waves-effect"><a href="#!">3</a></li>
+    <li class="waves-effect"><a href="#!">4</a></li>
+    <li class="waves-effect"><a href="#!">5</a></li>
+    <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
+  </ul>
 </div>
 
+</div>
+
+</div>
 
 <?php include "footer.php" ?>
