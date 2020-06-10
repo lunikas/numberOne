63e9bb2383da07f0da185fbe0e21dd85c138f93b
lunikas <lukasnikas90@gmail.com>
2020-06-10 19:35:49 +0300

    news su issokanciu modal

--- news.php
+++ news.php
@@ -1,10 +1,44 @@
-
 <?php include "header.php" ?>
 
 
 
-<!-- Čia kontaktų puslapio vidinė dalis -->
 
+<div class="container ">
+<h1>News</h1>
+
+<a class="waves-effect waves-light modal-trigger" href="#modal1">
+    <div class="row">
+
+    <div class="col s12 m4 l2"><img src="https://picsum.photos/150/150" alt=""></div>
+    <div class="col s12 m4 l8" ><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium odit velit soluta quam quibusdam non totam quasi, unde facilis culpa corporis expedita architecto possimus fugiat voluptate, similique nulla accusamus eos!</p></div>
+
+    <div id="modal1" class="modal">
+    <div class="modal-content">
+      <h4>Modal Header</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta temporibus delectus necessitatibus perferendis earum vero exercitationem ipsa iure voluptas incidunt quae cum ex quod quasi corporis eius aliquid, similique ipsum?Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti soluta cupiditate ea ut at reiciendis maiores possimus eum optio quo nihil sint porro, nostrum debitis alias natus ratione eligendi impedit?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam alias iusto corporis neque praesentium eos ipsa debitis. Eligendi est temporibus praesentium quis quo iusto alias cumque eum in exercitationem? Consequuntur!</p>
+    </div>
+    <div class="modal-footer">
+      <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
+    </div>
+  </div>
+
+    <!-- <div class="col s12 m4 l2"><button></button></div> -->
+  </div>
+  </a>
+
+<div class="row">
+    <div class="col s12 m4 l2"><img src="https://picsum.photos/150/150" alt=""></div>
+    <div class="col s12 m4 l8"><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium odit velit soluta quam quibusdam non totam quasi, unde facilis culpa corporis expedita architecto possimus fugiat voluptate, similique nulla accusamus eos!</p></div>
+    <div class="col s12 m4 l2"><button></button></div>
+  </div>
+
+
+<div class="row">
+    <div class="col s12 m4 l2"><img src="https://picsum.photos/150/150" alt=""></div>
+    <div class="col s12 m4 l8"><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium odit velit soluta quam quibusdam non totam quasi, unde facilis culpa corporis expedita architecto possimus fugiat voluptate, similique nulla accusamus eos!</p></div>
+    <div class="col s12 m4 l2"><button></button></div>
+  </div>
+</div>
 
 
 <?php include "footer.php" ?>
--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,13 @@
+// Bendras JS kodas
+
+// Luko JS kodas
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems, options);
+});
+
+
+
 var el = document.querySelector('.tabs');
     var instance = M.Tabs.init(el);
 
