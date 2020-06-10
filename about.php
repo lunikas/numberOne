50638b8e69aa2948da9256cf0866c702b445c7f0
JurgitaVisa <jurgita.visakaviciute@gmail.com>
2020-06-10 13:35:15 +0300

    about stiliai ir puslapis

--- about.php
+++ about.php
@@ -1,20 +1,57 @@
-
 <?php include "header.php" ?>
 
 <div class="container about">
-<h1>About Us</h1>
-
-<p>Mes mylim suniukus ir....</p>
-<img src="https://picsum.photos/id/237/500/500" alt="kazkas grazaus">
-
-<p>.... mokam lotyniskai ;) Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus eius, voluptatibus totam explicabo voluptatem nihil nisi consequuntur adipisci laboriosam debitis, rerum necessitatibus eligendi! Cum nam ipsum magni omnis. Iusto, itaque?</p>
-
-<p>Mus rasite Vilniaus centre</p>
-
-</div>
 
 
+    <div class="breadcrumb-wrapper">
+        <div class="col s12">
+            <a href="index.php" class="breadcrumb">Home</a>
+            <a href="about.php" class="breadcrumb">About</a>
+            <a href="#!" class="breadcrumb">Pirmas Vardas</a>
+        </div>
+    </div>
 
-<iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2306.4099714932604!2d25.27833385059149!3d54.68481288121817!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dd9411a1bd7cdb%3A0x645ec2d88e118d0c!2sLabdari%C5%B3%20g.%203%2C%20Vilnius%2001120!5e0!3m2!1sen!2slt!4v1591730100877!5m2!1sen!2slt"  frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
 
-<?php include "footer.php" ?>
+    <h1>About Us</h1>
+
+
+    <div class="card  light-blue-text text-darken-4">
+        
+        <div class="card-tabs">
+            <ul class="tabs tabs-fixed-width">
+                <li class="tab"><a class="active" href="#test4">Pirmas Vardas</a></li>
+                <li class="tab"><a href="#test5">Antras Vardas</a></li>
+                <li class="tab"><a href="#test6">Trečias vardas</a></li>
+            </ul>
+        </div>
+        <div class="card-content">
+            <div class="row">
+        <img class= "col l6 m10 s12" src="https://picsum.photos/id/237/500/500" alt="kazkas grazaus">
+        <p class= "col l6 m10 s12">Mes mokam lotyniskai --> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus eius, voluptatibus totam explicabo voluptatem nihil nisi consequuntur adipisci laboriosam debitis, rerum necessitatibus eligendi! Cum nam ipsum magni omnis. Iusto, itaque?</p>
+        </div>
+        </div>
+        <div class="card-content blue-grey lighten-5">
+            <div id="test4">Pirmas Vardas</div>
+            <div id="test5">Antras Vardas</div>
+            <div id="test6">Trečias Vardas</div>
+        </div>
+    </div>
+
+    <h5>Mus rasite Vilniaus centre</h5>
+    <div class="row">
+        <iframe class="map col l6" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2306.4099714932604!2d25.27833385059149!3d54.68481288121817!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dd9411a1bd7cdb%3A0x645ec2d88e118d0c!2sLabdari%C5%B3%20g.%203%2C%20Vilnius%2001120!5e0!3m2!1sen!2slt!4v1591730100877!5m2!1sen!2slt" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+        <div class="col l6">
+            <h6>Mūsų kontaktai: </h6>
+            <p>Kažkaip gražiai padėlioti</p>
+
+
+
+
+        </div>
+
+    </div>
+
+
+
+
+    <?php include "footer.php" ?>
