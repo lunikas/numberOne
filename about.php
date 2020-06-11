f5e0d0bc0d8cb7e2e3b31a2afa825e70249964b8
JurgitaVisa <jurgita.visakaviciute@gmail.com>
2020-06-11 17:34:57 +0300

    šiek tiek JS (Lukai, istryniau tavo kode options), siek tiek stiliu/spalvu

--- about.php
+++ about.php
@@ -33,8 +33,8 @@ if (isset($_GET["email"])) {
 
     <div class="breadcrumb-wrapper">
         <div class="col s12">
-            <a href="index.php" class="breadcrumb">Home</a>
-            <a href="about.php" class="breadcrumb">About</a>
+            <a href="index.php" class="tooltipped breadcrumb" data-tooltip="Grįžti į pradinį puslapį">Pagrindinis</a>
+            <a href="about.php" class="breadcrumb">Apie mus</a>
         </div>
     </div>
 
@@ -58,7 +58,7 @@ if (isset($_GET["email"])) {
         </div>
         <div class="card-content" id="one">
             <div class="row">
-                <img class="col l6  s12" src="https://picsum.photos/id/177/500/500" alt="kalnai">
+                <img class="materialboxed col l6  s12" src="https://picsum.photos/id/177/500/500" alt="kalnai">
 
                 <div class="col l6  s12 products">
                     <h6>Mėgstantiems aktyvų poilsį</h6>
@@ -69,7 +69,7 @@ if (isset($_GET["email"])) {
         </div>
         <div class="card-content" id="two">
             <div class="row">
-                <img class="col l6  s12" src="https://picsum.photos/id/191/500/500" alt="kelias">
+                <img class="materialboxed col l6  s12" src="https://picsum.photos/id/191/500/500" alt="kelias">
                 <div class="col l6  s12 products">
                     <h6>Trokštantiems įspūdžių ir norintiems pamatyti kuo daugiau</h6>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus eius, voluptatibus totam explicabo voluptatem nihil nisi consequuntur adipisci laboriosam debitis, rerum necessitatibus eligendi! Cum nam ipsum magni omnis. Iusto, itaque?</p>
@@ -80,7 +80,7 @@ if (isset($_GET["email"])) {
         <div class="card-content" id="three">
             <div class="row">
 
-                <img class="col l6  s12" src="https://picsum.photos/id/449/500/500" alt="papludimys">
+                <img class="materialboxed col l6  s12" src="https://picsum.photos/id/449/500/500" alt="papludimys">
 
                 <div class="col l6  s12 products">
                     <h6>Pavargusiems ir norintiems tiesiog pagulėti</h6>
